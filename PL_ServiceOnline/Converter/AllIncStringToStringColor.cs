bceae30dca36beeb15b0f22b7987c06265dc428d
Manuel <Nuel@NUEL-THINK>
2018-05-18 16:42:26 +0200

    ListView gestylt und umgestellt (Border, Height, Fontsize), Gridview (Fontsize)

---
+++ PL_ServiceOnline/Converter/AllIncStringToStringColor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PL_ServiceOnline.Converter
+{
+    class AllIncStringToStringColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if((string)value == "Ja")
+            {
+                return "LightCyan";
+            }
+            else
+            {
+                return "LightPink";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
