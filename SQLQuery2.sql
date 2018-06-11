d9bc39e915ccdae545689a25f84557e9fa723414
ManuelFre <manuel.freischlager@gmail.com>
2018-06-11 08:46:50 +0200

    Kleine Änderungen für Sync

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,11 +1,1 @@
-﻿delete from sow_user_delivery_address;
-delete from order_item_report_appendix;
-delete from order_item_report;
-delete from order_item;
-delete from service;
-delete from service_provider;
-delete from order_detail;
-delete from order_header;
-delete from sow_user;
-
-commit;
+﻿select * from order_item;
