ff725247db15723892b89bd5c19f0dc555357625
Manuel Grolitsch <wi16b097@technikum-wien.at>
2018-06-14 00:42:54 +0200

    Fixing DateProblem in OrderItemReport and Beautifying code

--- PL_ServiceOnline/CreateReportDialog.xaml.cs
+++ PL_ServiceOnline/CreateReportDialog.xaml.cs
@@ -11,10 +11,12 @@ using System.Windows.Media.Imaging;
 namespace PL_ServiceOnline
 {
     /// <summary>
-    /// Interaction logic for CreateReportDialog.xaml
+    /// CreateReportDialog is a Custom Dialog for adding multiple Pictures to the OrderItemReport by creating multiple OrderItemReportAppendices
+    /// Use Answer Property to get a full OrderItemReport_
     /// </summary>
     public partial class CreateReportDialog : Window
     {
+        #region Fields
         private bool pictureAdded = false;
         private bool changesMade = false;
         private int CurrentMaxOrderItemReportId;
@@ -22,34 +24,17 @@ namespace PL_ServiceOnline
 
         private BitmapImage appendixImage;
 
+        private int OrderItemId;
+        #endregion
+
+        #region Properties
         public List<OrderItemReportAppendix> AppendixImageList{ get; set; }
 
         //private BitmapImage emptyImage;
 
         //public ObservableCollection<BitmapImage> ImageList { get; set; }
 
-        private int OrderItemId;
 
-        public CreateReportDialog(int CurrentMaxOrderItemReportId, int orderItemId)
-        {
-            InitializeComponent();
-            btnDialogOk.IsEnabled = false;
-
-            lblImgCount.Content = imageCount.ToString();
-            lblDate.Content = DateTime.Now.ToString("dddd, dd. MMMM yyyy");
-            this.CurrentMaxOrderItemReportId = CurrentMaxOrderItemReportId;
-            OrderItemId = orderItemId;
-
-            AppendixImageList = new List<OrderItemReportAppendix>();
-
-            //emptyImage = new BitmapImage(new Uri(@"/Images/KeinBild.jpg", UriKind.Relative));
-
-            //ImageList = new ObservableCollection<BitmapImage>();
-            //ImageList.Add(emptyImage);
-
-            //ImageBox.ItemsSource = ImageList;
-
-        }
         public OrderItemReport_ Answer
         {
             get
@@ -66,8 +51,34 @@ namespace PL_ServiceOnline
                 };
             }
         }
+        #endregion
 
-        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        #region Constructor
+        public CreateReportDialog(int CurrentMaxOrderItemReportId, int orderItemId)
+        {
+            InitializeComponent();
+            btnDialogOk.IsEnabled = false;
+
+            lblImgCount.Content = imageCount.ToString();
+            //lblDate.Content = DateTime.Now.ToString("dddd, dd. MMMM yyyy"); too big for now
+            //lblDate.Content = DateTime.Now.ToString("dd. MMMM yyyy"); currently still too long for September...
+            lblDate.Content = DateTime.Now.ToString("dd. MMM. yyyy");
+            this.CurrentMaxOrderItemReportId = CurrentMaxOrderItemReportId;
+            OrderItemId = orderItemId;
+
+            AppendixImageList = new List<OrderItemReportAppendix>();
+
+            //prepared approach for showing all added images
+            //emptyImage = new BitmapImage(new Uri(@"/Images/KeinBild.jpg", UriKind.Relative));
+            //ImageList = new ObservableCollection<BitmapImage>();
+            //ImageList.Add(emptyImage);
+            //ImageBox.ItemsSource = ImageList;
+
+        }
+        #endregion
+
+        #region Gui Events
+        private void BtnDialogOk_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
         }
@@ -77,7 +88,7 @@ namespace PL_ServiceOnline
             txtComment.Focus();
         }
 
-        private void btnAddPicture_Click(object sender, RoutedEventArgs e)
+        private void BtnAddPicture_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
@@ -121,7 +132,7 @@ namespace PL_ServiceOnline
             }
         }
 
-        private void txtComment_TextChanged(object sender, TextChangedEventArgs e)
+        private void TxtComment_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (txtComment.Text.Equals(String.Empty) && !pictureAdded)
             {
@@ -134,5 +145,6 @@ namespace PL_ServiceOnline
                 btnDialogOk.IsEnabled = true;
             }
         }
+        #endregion
     }
 }
--- PL_ServiceOnline/ViewModel/DetailVm.cs
+++ PL_ServiceOnline/ViewModel/DetailVm.cs
@@ -15,19 +15,16 @@ using System.Windows.Media.Imaging;
 namespace PL_ServiceOnline.ViewModel
 {
     //TODO: fix multiple image view in orderitemreports xaml list
-    //TODO: make multiple Image upload in CreateReportDemo possible
+    //TODO: maybe add horizontal scrollbar into the reportitmes itemcontrol,... (already tried but isues with stackpannel approach)
     public class DetailVm : ViewModelBase
     {
+        #region Properties
         private IMessenger msg = Messenger.Default;
-
-        //Im Moment wird die Order Summary, die ausgewählt wurde, übergeben, danach wird diese nochmal in der DB abgefragt, damit auch Bilder etc. abgefragt werden können. Das fehlt! Es gibt im Moment nur den selben Inhalt mit selber id zurück.
         public OrderSummary SelectedJob { get; set; }
-
         public DetailedClass SelectedDetailed { get; set; }
         public RelayCommand BtnApplyChanges { get; set; }
         public RelayCommand BtnCreateReport { get; set; }
         public RelayCommand<OrderItemReport_> BtnAddPicture { get; set; }
-
         public Dataprovider Dp { get; set; }
 
 
@@ -83,29 +80,10 @@ namespace PL_ServiceOnline.ViewModel
 
         public double? AddittionalCost { get; set; }
 
-        //private string time;
-
-        //public string Time
-        //{
-        //    get { return time; }
-        //    set
-        //    {
-        //        int.TryParse(value.Split(':')[0], out int tmp);
-        //        Hour = tmp;
-        //        int.TryParse(value.Split(':')[1], out tmp);
-        //        Minute = tmp;
-        //        time = value;
-        //    }
-        //}
 
 
         public int Hour { get; set; }
         public int Minute { get; set; }
-
-
-        //public string StringPreferedDate { get; set; }
-        //public string StringOrderedDateTime { get; set; }
-
 
         private string serviceProviderComment;
 
@@ -114,17 +92,7 @@ namespace PL_ServiceOnline.ViewModel
             get { return serviceProviderComment; }
             set { serviceProviderComment = value; RaisePropertyChanged(); }
         }
-
         public string ServiceUnit { get; set; }
-
-        //TODO: IsFinished und IsConfirmed zusammenfassen und als Combobox ("Status:") anzeigen "nicht angenommen"(default wenn null, N, Nein), "abgelehnt" (msgBox=>confirm das der auftrag wirklich abgelehnt werden will), "angenommen" und "erledigt" 
-        //Auftrag löschen
-        //Gibt der Dienstleister im Schritt „Auftragsdetails bearbeiten“ bekannt, dass er den Auftrag ablehnen möchte, 
-        //so wird um erneute Bestätigung gebeten und der Auftrag als "abgelehnt" in der Datenbank persistiert.
-
-        /// <summary>
-        /// "nicht angenommen"(default wenn null, N, Nein), "abgelehnt" (msgBox=>confirm das der auftrag wirklich abgelehnt werden will), "angenommen" und "erledigt" 
-        /// </summary>
 
         private string status;
 
@@ -134,23 +102,15 @@ namespace PL_ServiceOnline.ViewModel
             set { status = value; RaisePropertyChanged(); }
         }
 
-
-        //public string Status { get; set; }
-
         public string[] AllStatuses { get; set; }
-
 
         public ObservableCollection<OrderItemReport_> OrderItemReports { get; set; }
 
-
-
         private DetailedClass OS { get; set; }
 
+        #endregion
 
 
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
         public DetailVm()
         {
             CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("de-DE");
@@ -188,24 +148,19 @@ namespace PL_ServiceOnline.ViewModel
                         foreach (var item in SelectedDetailed.OrderItemReports)
                         {
                             item.Visibility = "Collapsed";
-                            
+
                             if (p == item)
                             {
-                                //MessageBox.Show($"Id= {p.Id},  OrderItemId= { p.OrderItemId}"); //just to check if the correct parameters actually arrive
-                                //SelectedDetailed.OrderItemReports.Add(new OrderItemReportAppendix()
-                                //item.Appendix.Add(new OrderItemReportAppendix()
-                                SelectedDetailed.OrderItemReports[i].Appendix.Add(new OrderItemReportAppendix()                                
+                                SelectedDetailed.OrderItemReports[i].Appendix.Add(new OrderItemReportAppendix()
                                 {
                                     Id = item.OrderItemId,
                                     Picture = ImageConverter.ImageToByteArray(new BitmapImage(new Uri(openFileDialog.FileName, UriKind.Absolute)))
 
                                 });
-                                //MessageBox.Show(SelectedDetailed.OrderItemReports[i].Appendix.ToString() + "     " + i);
-                                
-                                item.Visibility = "Visible"; //TODO: Why does this not work?
+
+                                item.Visibility = "Visible"; //TODO: Does it work?
                                 RaisePropertyChanged(nameof(SelectedDetailed));
 
-                                //Dp.UpdateOrderItemReportAppendix(SelectedDetailed); //TODO: In that methode update all OrderItemReports or just this one...
                             }
                             RaisePropertyChanged(nameof(item.Visibility));
                             i++;
@@ -215,13 +170,10 @@ namespace PL_ServiceOnline.ViewModel
                 });
 
             //CreateDemoData();
-
-
         }
 
         private void AppendDocuments()
         {
-
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
                 Title = "Select a picture",
@@ -246,25 +198,16 @@ namespace PL_ServiceOnline.ViewModel
                     };
                     SelectedDetailed.OrderItemReports.Add(oir);
                     Dp.AddOrderItemReport(oir);
-                    //if(Dp.AddOrderItemReport(oir))
-                    //    MessageBox.Show("Update erfolgreich!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
-                    //else
-                    //    MessageBox.Show("Update fehlgeschlagen", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
-
 
                     if (Dp.UpdateOrderItemData(SelectedDetailed))
                         MessageBox.Show("Update erfolgreich!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
                     else
                         MessageBox.Show("Update fehlgeschlagen", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
-
-
                 }
                 catch (Exception e)
                 {
-
                     MessageBox.Show("Laden des Bildes fehlgeschlagen!\n" + e.Message, "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
-
             }
         }
 
@@ -276,21 +219,144 @@ namespace PL_ServiceOnline.ViewModel
                 if (item.OrderItemId > CurrentMaxOrderItemReportId)
                     CurrentMaxOrderItemReportId = item.OrderItemId;
             }
-            CreateReportDialog reportDialog = new CreateReportDialog(CurrentMaxOrderItemReportId+1,OrderItemId);
+            CreateReportDialog reportDialog = new CreateReportDialog(CurrentMaxOrderItemReportId + 1, OrderItemId);
             if (reportDialog.ShowDialog() == true)
             {
                 OrderItemReport_ CreatedReport = reportDialog.Answer;
                 OrderItemReports.Add(CreatedReport);
                 Dp.AddOrderItemReport(CreatedReport);
-                
 
                 RaisePropertyChanged(nameof(SelectedDetailed.OrderItemReports));
-                //TODO:Since hard reset after merge fail,... new reports are not shown anymore,.. why?
-          
-
-                //MessageBox.Show(reportDialog.Answer.ToString());
             }
         }
+
+        private void ApplyChanges()
+        {
+            if (Status.Equals(AllStatuses[3]))
+            {
+                if (MessageBox.Show("Wollen Sie den Auftrag wirklich ablehnen?", "Auftrag ablehnen?", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+                {
+                    return;
+                }
+            }
+
+            SelectedDetailed.AddittionalCost = AddittionalCost;
+            SelectedDetailed.Finalprice = Finalprice;
+            SelectedDetailed.IsAllInclusive = IsAllInclusive;
+
+            SelectedDetailed.IsConfirmed = GetConfirmStatus(Status);
+            SelectedDetailed.IsFinished = GetFinishedStatus(Status);
+
+            SelectedDetailed.PreferedDate = PreferedDate;
+            SelectedDetailed.ServiceProviderComment = ServiceProviderComment;
+
+
+            if (IsConfirmed != null && IsConfirmed != "x" && PreferedDate < DateTime.Now)
+                msg.Send(new GenericMessage<string>("past"));
+            else if (IsConfirmed != null && IsConfirmed != "x" && PreferedDate > DateTime.Now)
+                msg.Send(new GenericMessage<string>("future"));
+            else
+                msg.Send(new GenericMessage<string>("denied"));
+
+
+            if (Dp.UpdateOrderItemData(SelectedDetailed))
+            {
+                MessageBox.Show("Update erfolgreich!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                RaisePropertyChanged(nameof(PreferedDate)); //so that the PreferedDate in the DetailView gets actually updated once it's sent to the DB
+                msg.Send(new GenericMessage<string>("update"));     //Damit die Auswertung der Firmendaten mitbekommt, dass es eine Änderung gibt.
+            }
+            else
+                MessageBox.Show("Update fehlgeschlagen", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        private string GetStatus(string isFinished, string isConfirmed)
+        {
+            if (isConfirmed != null)
+            {
+                if (isConfirmed.Equals("x") || isConfirmed.Equals("X"))
+                {
+                    return AllStatuses[3];
+                }
+
+                if (IsFinished != null)
+                {
+                    if (isFinished.Equals("Y") || isFinished.Equals("Ja"))
+                    {
+                        return AllStatuses[0];//Abgeschlossen
+                    }
+                    else if (isConfirmed.Equals("Y") || isConfirmed.Equals("Ja"))
+                    {
+                        return AllStatuses[1]; // Angenommen
+                    }
+                }
+            }
+            return AllStatuses[2]; //Nicht bestätigt"
+        }
+        private string GetFinishedStatus(string status)
+        {
+            //wenn status in checkbox == Abgeschlossen [0]
+            if (status != null)
+            {
+                if (status.Equals(AllStatuses[0]))
+                    return "Y";
+            }
+            return "N";
+        }
+
+        private string GetConfirmStatus(string status)
+        {
+            //wenn status in checkbox == Abgeschlossen [0] oder Angenommen [1]  dann 'Y'
+            if (status != null)
+            {
+                if (status.Equals(AllStatuses[3]))
+                    return "x";
+                if (status.Equals(AllStatuses[0]) || status.Equals(AllStatuses[1]))
+                    return "Y";
+            }
+            return "N";
+        }
+
+        private void ChangeSelected(GenericMessage<OrderSummary> obj)
+        {
+            SelectedJob = obj.Content;
+
+
+            if (SelectedJob != null)
+            {
+                SelectedDetailed = OS.GetDetailView(SelectedJob);
+
+                CustomerId = SelectedDetailed.CustomerId;
+                Firstname = SelectedDetailed.Firstname;
+                Lastname = SelectedDetailed.Lastname;
+                Address = SelectedDetailed.Address;
+                Zip = SelectedDetailed.Zip;
+                City = SelectedDetailed.City;
+                Phone = SelectedDetailed.Phone;
+                Email = SelectedDetailed.Email;
+                OrderItemId = SelectedDetailed.OrderItemId;
+                OrderId = SelectedDetailed.OrderId;
+                PreferedDate = SelectedDetailed.PreferedDate;
+                Servicedescription = SelectedDetailed.Servicedescription;
+                BookedItems = SelectedDetailed.BookedItems;
+                IsAllInclusive = SelectedDetailed.IsAllInclusive;
+                Finalprice = SelectedDetailed.Finalprice;
+                OrderedDateTime = SelectedDetailed.OrderedDateTime;
+                CustomerNotice = SelectedDetailed.CustomerNotice;
+                IsFinished = SelectedDetailed.IsFinished;
+                IsConfirmed = SelectedDetailed.IsConfirmed;
+                AddittionalCost = SelectedDetailed.AddittionalCost;
+                ServiceProviderComment = SelectedDetailed.ServiceProviderComment;
+                ServiceUnit = SelectedDetailed.ServiceUnit;
+                OrderItemReports = new ObservableCollection<OrderItemReport_>(SelectedDetailed.OrderItemReports as List<OrderItemReport_>);
+                Status = GetStatus(IsFinished, IsConfirmed);
+            }
+
+        }
+        public void StartSync()
+        {
+            SyncFromBackend SFB = new SyncFromBackend();
+            MessageBox.Show(SFB.StartSync().ToString());
+        }
+
 
         private void CreateDemoData()
         {
@@ -369,162 +435,5 @@ namespace PL_ServiceOnline.ViewModel
             };
 
         }
-
-
-        private void ApplyChanges()
-        {
-            if (Status.Equals(AllStatuses[3]))
-            {
-                if (MessageBox.Show("Wollen Sie den Auftrag wirklich ablehnen?", "Auftrag ablehnen?", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
-                {
-                    return;
-                }
-            }
-            //OS.AddittionalCost = AddittionalCost;
-
-
-
-            SelectedDetailed.AddittionalCost = AddittionalCost;
-            SelectedDetailed.Finalprice = Finalprice;
-            SelectedDetailed.IsAllInclusive = IsAllInclusive;
-            //SelectedDetailed.IsConfirmed = IsConfirmed;
-            //SelectedDetailed.IsFinished = IsFinished;
-            SelectedDetailed.IsConfirmed = GetConfirmStatus(Status);
-            SelectedDetailed.IsFinished = GetFinishedStatus(Status);
-
-            SelectedDetailed.PreferedDate = PreferedDate;
-            SelectedDetailed.ServiceProviderComment = ServiceProviderComment;
-
-
-            if(IsConfirmed != null && IsConfirmed != "x" && PreferedDate < DateTime.Now)
-                msg.Send<GenericMessage<string>>(new GenericMessage<string>("past"));
-            else if (IsConfirmed != null && IsConfirmed != "x" && PreferedDate > DateTime.Now)
-                msg.Send<GenericMessage<string>>(new GenericMessage<string>("future"));
-            else
-                msg.Send<GenericMessage<string>>(new GenericMessage<string>("denied"));
-
-
-            if (Dp.UpdateOrderItemData(SelectedDetailed))
-            {
-                MessageBox.Show("Update erfolgreich!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
-                RaisePropertyChanged(nameof(PreferedDate)); //so that the PreferedDate in the DetailView gets actually updated once it's sent to the DB
-                msg.Send<GenericMessage<string>>(new GenericMessage<string>("update"));     //Damit die Auswertung der Firmendaten mitbekommt, dass es eine Änderung gibt.
-            }
-            else
-                MessageBox.Show("Update fehlgeschlagen", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
-
-
-        private string GetStatus(string isFinished, string isConfirmed)
-        {
-            if (isConfirmed != null)
-            {
-                if (isConfirmed.Equals("x") || isConfirmed.Equals("X"))
-                {
-                    return AllStatuses[3];
-                }
-
-                if (IsFinished != null)
-                {
-                    if (isFinished.Equals("Y") || isFinished.Equals("Ja"))
-                    {
-                        return AllStatuses[0];//Abgeschlossen
-                    }
-                    else if (isConfirmed.Equals("Y") || isConfirmed.Equals("Ja"))
-                    {
-                        return AllStatuses[1]; // Angenommen
-                    }
-                }
-            }
-
-
-            return AllStatuses[2]; //Nicht bestätigt"
-        }
-        private string GetFinishedStatus(string status)
-        {
-            //wenn status in checkbox == Abgeschlossen [0]
-            if (status != null)
-            {
-                if (status.Equals(AllStatuses[0]))
-                    return "Y";
-            }
-            return "N";
-        }
-
-        private string GetConfirmStatus(string status)
-        {
-            //wenn status in checkbox == Abgeschlossen [0] oder Angenommen [1]  dann 'Y'
-            if (status != null)
-            {
-                if (status.Equals(AllStatuses[3]))
-                    return "x";
-                if (status.Equals(AllStatuses[0]) || status.Equals(AllStatuses[1]))
-                    return "Y";
-            }
-            return "N";
-        }
-
-        private void ChangeSelected(GenericMessage<OrderSummary> obj)
-        {
-            //TODO: proper view for OrderItemReport
-
-
-            SelectedJob = obj.Content;
-
-
-            //AddittionalCost;
-            //Finalprice;
-            //IsAllInclusive;
-            //sConfirmed;
-            //IsFinished;
-            //PreferedDate;
-            //ServiceProviderComment;
-
-            //.Contains(",") ? AddittionalCost.Replace(',', '.') : AddittionalCost  .Replace('.', ',')
-
-
-            if (SelectedJob != null)
-            {
-
-                SelectedDetailed = OS.GetDetailView(SelectedJob);
-
-                CustomerId = SelectedDetailed.CustomerId;
-                Firstname = SelectedDetailed.Firstname;
-                Lastname = SelectedDetailed.Lastname;
-                Address = SelectedDetailed.Address;
-                Zip = SelectedDetailed.Zip;
-                City = SelectedDetailed.City;
-                Phone = SelectedDetailed.Phone;
-                Email = SelectedDetailed.Email;
-                OrderItemId = SelectedDetailed.OrderItemId;
-                OrderId = SelectedDetailed.OrderId;
-                PreferedDate = SelectedDetailed.PreferedDate;
-                Servicedescription = SelectedDetailed.Servicedescription;
-                BookedItems = SelectedDetailed.BookedItems;
-                IsAllInclusive = SelectedDetailed.IsAllInclusive;
-                Finalprice = SelectedDetailed.Finalprice;
-                OrderedDateTime = SelectedDetailed.OrderedDateTime;
-                CustomerNotice = SelectedDetailed.CustomerNotice;
-                IsFinished = SelectedDetailed.IsFinished;
-                IsConfirmed = SelectedDetailed.IsConfirmed;
-                AddittionalCost = SelectedDetailed.AddittionalCost;
-                ServiceProviderComment = SelectedDetailed.ServiceProviderComment;
-                ServiceUnit = SelectedDetailed.ServiceUnit;
-                OrderItemReports = new ObservableCollection<OrderItemReport_>(SelectedDetailed.OrderItemReports as List<OrderItemReport_>);
-                Status = GetStatus(IsFinished, IsConfirmed);
-                //StringOrderedDateTime = OrderedDateTime.ToString("dd.MM.yyyy - hh.mm");
-                //StringPreferedDate = PreferedDate.ToString("dd.MM.yyyy - hh.mm");
-            }
-
-        }
-
-
-
-        public void StartSync()
-        {
-            SyncFromBackend SFB = new SyncFromBackend();
-            MessageBox.Show(SFB.StartSync().ToString());
-        }
-
     }
 }
